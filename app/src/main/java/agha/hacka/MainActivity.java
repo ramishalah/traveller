8d7eaca0d15232a70a4231c6143ec044fc83a2dd
rami shelh <romioo1010@gmail.com>
2018-04-04 20:55:43 +0300

    adding the hello variable

--- app/src/main/java/agha/hacka/MainActivity.java
+++ app/src/main/java/agha/hacka/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     // rami
     String rami = "fasdfa";
+
+    String hello = "fadsjlkfsa";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
