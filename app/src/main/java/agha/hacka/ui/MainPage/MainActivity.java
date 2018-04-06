e0cdaa832acbe3153b69d65544d1fc36ff3553c7
rami shelh <romioo1010@gmail.com>
2018-04-06 14:55:18 +0300

    adding the butter knife

--- app/src/main/java/agha/hacka/ui/MainPage/MainActivity.java
+++ app/src/main/java/agha/hacka/ui/MainPage/MainActivity.java
@@ -3,13 +3,17 @@ package agha.hacka.ui.MainPage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.List;
 
 import agha.hacka.R;
 import agha.hacka.pogo.FacultyResponse;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements MainPageView{
+    @BindView(R.id.helloWorld) TextView helloWorld;
     private static final String TAG = "MainActivity";
 
     private MainPagePresenter mainPagePresenter;
@@ -21,9 +25,12 @@ public class MainActivity extends AppCompatActivity implements MainPageView{
         Log.d(TAG, "onCreate: Starting onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         mainPagePresenter = new MainPagePresenter(this, this);
         mainPagePresenter.getFaculty();
+
+        helloWorld.setText("butter knife is used");
     }
 
     @Override
